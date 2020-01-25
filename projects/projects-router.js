259428556b7411b989785068981d600969d03fa0
Code4Blessings <robinwarden@code4blessings.com>
2020-01-25 15:03:08 -0500

    Get project by id tested successfully

--- projects/projects-router.js
+++ projects/projects-router.js
@@ -20,4 +20,47 @@ router.get('/', (req, res) => {
     })
 })
 
+//Retrieve project list by id
+
+router.get('/:id', (req, res) => {
+    const id = req.params.id
+    dBase('projects').where({
+            id: id
+        }).select('id')
+        .then(projectId => {
+            res.status(200).json(projectId)
+        })
+        .catch(err => {
+            res.status(500).json({
+                errorMessage: "Project ID cannot be retrieved",
+                message: err.message
+            })
+        })
+})
+
+//Add An Account
+router.post('/', (req, res) => {
+    const data = {
+        name: req.body.name,
+        budget: req.body.budget
+    };
+    dBase('accounts').insert(data)
+        .then(postedAccount => {
+            dBase('accounts').where({
+                    id: postedAccount[0]
+                }).first()
+                .then(newAccount => {
+                    res.status(201).json(newAccount);
+                })
+                .catch(err => {
+                    console.log(err);
+                    res.status(500).json({
+                        errorMessage: "Account could not be created",
+                        message: err.message
+                    });
+                });
+        })
+
+});
+
 module.exports = router;
